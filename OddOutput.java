0d8527fd173f6b8fbe12dafce09746810a6003e1
Kirill <90597372+KirillKurnakov@users.noreply.github.com>
2021-10-31 23:23:29 +0300

    Add files via upload

---
+++ OddOutput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// 12 задание (вывод нечетных чисел )
+public class OddOutput extends Main {
+    public void Odds() {
+        Scanner in = new Scanner(System.in);
+        int hg = in.nextInt();
+        if (hg == 0)
+            System.exit(0);
+        if (hg % 2 == 1) {
+            System.out.println(hg);
+            Odds();
+        }
+        else
+            Odds();
+        //return Odds();
+    }
+}
